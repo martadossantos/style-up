7b386c4c14a494e854e8ad39eca002a48f40fd91
martadossantos <hello@martasantos.co.uk>
2020-11-17 12:47:35 +0000

    mobile test

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
+
+
+
+
 let dragEls = document.querySelectorAll('.drag')
 
 dragEls.forEach(el => {
    dragElement(el);
+   moveElement(el);
 })
 
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
-   // if present, the header is where you move the DIV from:
-   document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
- } else {
-   // otherwise, move the DIV from anywhere inside the DIV:
-   elmnt.onmousedown = dragMouseDown;
- }
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
 
   function dragMouseDown(e) {
     e = e || window.event;
@@ -43,5 +48,45 @@ function dragElement(elmnt) {
     // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
+  }
+}
+
+
+function moveElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").ontouchstart = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.ontouchstart = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.ontouchend = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.ontouchmove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.ontouchend = null;
+    document.ontouchmove = null;
   }
 }
