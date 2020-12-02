3b4728a5d6c26357243d08f13c2ba7f09fade611
martadossantos <hello@martasantos.co.uk>
2020-12-02 19:06:08 +0000

    resized images and js for random spawn

--- js/main.js
+++ js/main.js
@@ -1,13 +1,47 @@
-var draggableElems = document.querySelectorAll('.drag');
+let draggableElems = document.querySelectorAll('.drag');
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+draggableElems.forEach(el => {
+
+  let wW = window.innerWidth;
+  let wH = window.innerHeight;
+  let half_W = wW / 2;
+
+  let randomX = getRandomInt(60, (wW - (wW / 8.5)));
+  let randomY = getRandomInt(40, (wH - (wH / 2.75)));
+
+  if (randomX < (half_W - 150) || randomX > (half_W + 150)) {
+    el.style.left = randomX + 'px';
+  } else {
+
+    if (randomX > half_W) {
+      let newX = randomX + 200;
+      el.style.left = newX + 'px';
+    } else {
+      let newX = randomX - 400;
+      el.style.left = newX + 'px';
+    }
+    
+  }
+
+  el.style.top = randomY + 'px';
+
+})
 
 // array of Draggabillies
-var draggies = []
+let draggies = []
 
 // init Draggabillies
-for ( var i=0; i < draggableElems.length; i++ ) {
-  var draggableElem = draggableElems[i];
-  var draggie = new Draggabilly( draggableElem, {
+for ( let i=0; i < draggableElems.length; i++ ) {
+  let draggableElem = draggableElems[i];
+  let draggie = new Draggabilly( draggableElem, {
     // options...
   });
+  
   draggies.push( draggie );
 }
